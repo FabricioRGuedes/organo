23df5efef51dcc4afc8a12486aa7d71b36b42aa1
FabricioRGuedes <fabricio.r.guedes@gmail.com>
2023-04-18 16:25:36 -0300

    Deploy

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,8 +5,10 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select onChange={e => props.aoAlterado(e.target.value)} required={props.required} value={props.valor}>
-                {props.itens.map(item => <option key={item}>{item}</option>)}
-
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
             </select>
         </div>
     )
